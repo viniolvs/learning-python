138aacac1720f59442d86dd3c9ed102cb8b8dbab
Vinicius <vinioliveira.s@hotmail.com>
2022-02-06 19:33:30 -0300

    Testing exercise

--- Testing/city_functions.py
+++ Testing/city_functions.py
@@ -1,3 +1,5 @@
-def city_country(city, coutry):
-    return (city.title() + ", " + coutry.title())
+def city_country(city, coutry, population=0):
+    if population==0:
+        return (city.title() + ", " + coutry.title())
+    return (city.title() + ", " + coutry.title() + " - população " + str(population))
 
--- Testing/test_city.py
+++ Testing/test_city.py
@@ -2,8 +2,11 @@ import unittest
 from city_functions import city_country 
 
 class TestCityFunctions(unittest.TestCase):
+    def test_city_country_population(self):
+        formatted = city_country("cascavel", "brasil", 300000)
+        self.assertEqual(formatted, "Cascavel, Brasil - população 300000")
     def test_city_country(self):
-        formatted = city_country("Cascavel", "Brasil")
+        formatted = city_country("cascavel", "brasil")
         self.assertEqual(formatted, "Cascavel, Brasil")
 
 unittest.main()
